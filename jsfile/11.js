c57d131f6de49ce6156bd109b73d75cfe937f964
Alexandr Voronkov <alebymars@ya.ru>
2023-05-27 22:18:42 +0700

    lab8

--- jsfile/11.js
+++ jsfile/11.js
@@ -3,22 +3,82 @@ import { getData } from "./humansData.js";
 function allData() {
   getData()
     .then((res) => {
-      console.log(res);
-
-      const main = document.getElementById("main");
-
-      while (main.firstChild) {
-        main.firstChild.removeChild(main.firstChild);
-      }
-
-      const p = document.createElement("p");
-      p.innerHTML = `<p>${JSON.stringify(res)}</p>`;
-      main.appendChild(p);
+      createTable(res);
     })
     .catch((e) => {
       console.log(e);
     });
 }
 
+//
+const createTable = (data) => {
+  const tableId = document.getElementById("main");
+
+  let tableRow = [];
+  data.map(({ lastName, name, age, gender, address, telephone }, index) => {
+    tableRow.push(`
+      <tr id='${index}'>
+          <td>${name}</td>
+          <td>${lastName}</td>
+          <td id="age${index}">${age}</td>
+          <td>${gender}</td>
+          <td>${address}</td>
+          <td>${telephone}</td>
+          <td><input type="button" id="deleteButton" class="deleteButton" value="Удалить" onclick="deleteRow(${index})"></td>
+      </tr>
+  `);
+  });
+
+  const tableData = `
+    <table id="peopleTable" class="peopleTable">
+      <tr>
+          <td colspan="7">Человек</td>
+      </tr>
+          ${tableRow}
+      </table>
+  `;
+
+  tableId.innerHTML = tableData;
+  document.body.appendChild(tableId);
+  replaceColor();
+};
+//
+
+function replaceColor() {
+  let row = document.querySelectorAll("tr");
+  for (let i = 1; i < row.length; i++) {
+    let agePeople = row[i].cells[2].textContent;
+    if (agePeople < 18) {
+      row[i].style.backgroundColor = "#69ff52";
+    } else if (agePeople > 18 && agePeople < 60) {
+      row[i].style.backgroundColor = "#fffc52";
+    } else {
+      row[i].style.backgroundColor = "#f24141";
+    }
+  }
+}
+
+function deleteRow(id) {
+  const table = document.getElementById("peopleTable");
+  const rows = table.getElementsByTagName("tr");
+
+  const idToDelete = id.toString(); // id записи для удаления
+  let rowToDelete = null;
+
+  for (let i = 1; i < rows.length; i++) {
+    const id = rows[i].id;
+
+    if (id === idToDelete) {
+      rowToDelete = rows[i];
+      break;
+    }
+  }
+
+  if (rowToDelete) {
+    rowToDelete.parentNode.removeChild(rowToDelete);
+  }
+}
+
 window.allData = allData;
-export default { allData };
+window.deleteRow = deleteRow;
+export default { allData, deleteRow };
